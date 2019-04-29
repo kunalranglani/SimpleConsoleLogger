6a01e30510a39a02464e714dbf64d9c89e627bf3
Kunal Ranglani <kunalranglani@hotmail.com>
2019-04-29 16:53:20 -0700

    adding comments for logger methods

--- SimpleConsoleLogger/SimpleConsoleLogger/ConsoleLogger.cs
+++ SimpleConsoleLogger/SimpleConsoleLogger/ConsoleLogger.cs
@@ -5,21 +5,35 @@ using System.Text;
 
 namespace SimpleConsoleLogger
 {
+    // Static logger for the app
     public static class ConsoleLogger
     {
         private readonly static StringBuilder log = new StringBuilder();
-        public static List<Line> lines = new List<Line>();
+        private static List<Line> lines = new List<Line>();
 
+        /// <summary>
+        /// Logs a message to the console in white color.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         public static void Log(string message)
         {
             LogMessage(message, ConsoleColor.White, Level.Normal);
         }
 
+        /// <summary>
+        /// Logs a message to the console in green color.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         public static void LogSuccess(string message)
         {
             LogMessage(message, ConsoleColor.Green, Level.Success);
         }
 
+        /// <summary>
+        /// Logs an exception to the console in red color.
+        /// </summary>
+        /// <param name="exception">The exception to log.</param>
+        /// <param name="exitApplication">Exit the application.</param>
         public static void LogException(Exception exception, bool exitApplication = false)
         {
             LogMessage(exception.Message, ConsoleColor.Red, Level.Error);
@@ -31,6 +45,11 @@ namespace SimpleConsoleLogger
             }
         }
 
+        /// <summary>
+        /// Logs a message to the console in red color.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exitApplication">Exit the application.</param>
         public static void LogError(string message, bool exitApplication = false)
         {
             LogMessage(message, ConsoleColor.Red, Level.Error);
@@ -41,26 +60,46 @@ namespace SimpleConsoleLogger
             }
         }
 
+        /// <summary>
+        /// Logs a message to the console in yellow color.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         public static void LogWarning(string warning)
         {
             LogMessage(warning, ConsoleColor.Yellow, Level.Warning);
         }
 
+        /// <summary>
+        /// Log a silent message to the logs but does not output to console.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         public static void LogSilent(string message)
         {
             LogMessage(message, ConsoleColor.White, Level.Silent);
         }
 
+        /// <summary>
+        /// Gets a string value to all the messages that have been logged.
+        /// </summary>
+        /// <returns></returns>
         public static string GetLogsRawString()
         {
             return log.ToString();
         }
 
+        /// <summary>
+        /// Gets am array of all lines that have been logged.
+        /// </summary>
+        /// <returns></returns>
         public static Line[] GetLogs()
         {
             return lines.ToArray();
         }
 
+        /// <summary>
+        /// Saves the logs logged so far to a file.
+        /// </summary>
+        /// <param name="logFileFullPath">The full file path including file name.</param>
         public static void LogToFile(string logFileFullPath)
         {
             try
--- SimpleConsoleLogger/SimpleConsoleLogger/ILogger.cs
+++ SimpleConsoleLogger/SimpleConsoleLogger/ILogger.cs
@@ -4,22 +4,60 @@ namespace SimpleConsoleLogger
 {
     public interface ILogger
     {
+        /// <summary>
+        /// Logs a message to the console in white color.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         void Log(string message);
 
+        /// <summary>
+        /// Logs a message to the console in green color.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         void LogSuccess(string message);
 
+        /// <summary>
+        /// Logs an exception to the console in red color.
+        /// </summary>
+        /// <param name="exception">The exception to log.</param>
+        /// <param name="exitApplication">Exit the application.</param>
         void LogException(Exception exception, bool exitApplication = false);
 
+        /// <summary>
+        /// Logs a message to the console in red color.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exitApplication">Exit the application.</param>
         void LogError(string message, bool exitApplication = false);
 
+        /// <summary>
+        /// Logs a message to the console in yellow color.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         void LogWarning(string warning);
 
+        /// <summary>
+        /// Log a silent message to the logs but does not output to console.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         void LogSilent(string message);
 
+        /// <summary>
+        /// Saves the logs logged so far to a file.
+        /// </summary>
+        /// <param name="logFileFullPath">The full file path including file name.</param>
         void LogToFile(string logFileFullPath);
 
+        /// <summary>
+        /// Gets a string value to all the messages that have been logged.
+        /// </summary>
+        /// <returns></returns>
         string GetLogsRawString();
 
+        /// <summary>
+        /// Gets am array of all lines that have been logged.
+        /// </summary>
+        /// <returns></returns>
         Line[] GetLogs();
     }
 }
